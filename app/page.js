8d439722fed79d9a217674446bb34f75f6e7480f
Liv Clarence A. Torres <149602529+DontReb@users.noreply.github.com>
2025-09-10 22:32:17 +0800

    update

--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
 'use client'
 import Navbar from "@/components/Navbar";
+import { Ovo, Outfit } from "next/font/google";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"]
+});
+
+const ovo = Ovo({
+  subsets: ["latin"],
+  weight: ["400"]
+});
 
 export default function Home() {
   return (
     <>
-    <Navbar />
+    <Navbar ovoClass={ovo.className} outfitClass={outfit.className} />
     </>
   );
 }
-
-// 18:26
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,24 +2,27 @@ import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = (props) => {
+
+    const { ovoClass, outfitClass } = props
+
     return (
         <>
-            <nav>
-                <a href="">
-                    <Image src={assets.logo} className='w-28 cursor-pointer mr-14' />
+            <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+                <a href="#top">
+                    <Image src={assets.logo} alt="Logo of the person" className='w-28 cursor-pointer mr-14' />
                 </a>
 
-                <ul>
-                    <li><a href='#top'>Home</a></li>
-                    <li><a href='#about'>About me</a></li>
-                    <li><a href='#services'>Services</a></li>
-                    <li><a href='#work'>My Work</a></li>
-                    <li><a href='#contact'>Contact Me</a></li>
+                <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+                    <li><a className={ovoClass} href='#top'>Home</a></li>
+                    <li><a className={ovoClass} href='#about'>About me</a></li>
+                    <li><a className={ovoClass} href='#services'>Services</a></li>
+                    <li><a className={ovoClass} href='#work'>My Work</a></li>
+                    <li><a className={ovoClass} href='#contact'>Contact Me</a></li>
                 </ul>
 
                 <div>
-                    <a href="#contact">Contact <Image src={assets.arrow_icon} className='w-3' /></a>
+                    <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4' >Contact <Image src={assets.arrow_icon} alt="Arrow to contacts" className='w-3' /></a>
                 </div>
             </nav>
         </>
