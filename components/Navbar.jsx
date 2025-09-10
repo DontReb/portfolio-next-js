3fa4f3a0ac256f1c15f3a09bdba059e8fe640e5b
Liv Clarence A. Torres <149602529+DontReb@users.noreply.github.com>
2025-09-10 23:34:49 +0800

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,20 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Navbar = (props) => {
 
     const { ovoClass, outfitClass } = props
+
+    const sideMenuRef = useRef();
+    
+    const openMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+    
+    const closeMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
 
     return (
         <>
@@ -32,23 +42,24 @@ const Navbar = (props) => {
 
                     <a href="#contact" className={`hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 ${ovoClass}`} >Contact <Image src={assets.arrow_icon} alt="Arrow to contacts" className='w-3' /></a>
 
-                    <button className='block md:hidden ml-3'>
+                    <button className='block md:hidden ml-3' onClick={openMenu}>
                         <Image src={assets.menu_black} alt="Hamburger icon for menu toggle" className='w-6' />
                     </button>
                 </div>
 
                 {/* mobile menu */}
 
-                <ul className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+                <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
 
-                    <div className='absolute right-6 top-6'>
+                    <div className='absolute right-6 top-6' onClick={closeMenu}>
                         <Image src={assets.close_black} alt="button to close the menu" className='w-5 cursor-pointer' />
                     </div>
-                    <li><a className={ovoClass} href='#top'>Home</a></li>
-                    <li><a className={ovoClass} href='#about'>About me</a></li>
-                    <li><a className={ovoClass} href='#services'>Services</a></li>
-                    <li><a className={ovoClass} href='#work'>My Work</a></li>
-                    <li><a className={ovoClass} href='#contact'>Contact Me</a></li>
+
+                    <li><a className={ovoClass} onClick={closeMenu} href='#top'>Home</a></li>
+                    <li><a className={ovoClass} onClick={closeMenu} href='#about'>About me</a></li>
+                    <li><a className={ovoClass} onClick={closeMenu} href='#services'>Services</a></li>
+                    <li><a className={ovoClass} onClick={closeMenu} href='#work'>My Work</a></li>
+                    <li><a className={ovoClass} onClick={closeMenu} href='#contact'>Contact Me</a></li>
                 </ul>
             </nav>
         </>
