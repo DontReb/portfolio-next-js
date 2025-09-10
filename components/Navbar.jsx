7dc35582df115670da3c4d0512525c2a0229bcb4
Liv Clarence A. Torres <149602529+DontReb@users.noreply.github.com>
2025-09-10 23:00:33 +0800

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,12 +8,15 @@ const Navbar = (props) => {
 
     return (
         <>
+            <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+                <Image src={assets.header_bg_color} alt="Header gradient background image" className='w-full' />
+            </div>
             <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
                 <a href="#top">
                     <Image src={assets.logo} alt="Logo of the person" className='w-28 cursor-pointer mr-14' />
                 </a>
 
-                <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+                <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-xs bg-opacity-50'>
                     <li><a className={ovoClass} href='#top'>Home</a></li>
                     <li><a className={ovoClass} href='#about'>About me</a></li>
                     <li><a className={ovoClass} href='#services'>Services</a></li>
@@ -21,8 +24,17 @@ const Navbar = (props) => {
                     <li><a className={ovoClass} href='#contact'>Contact Me</a></li>
                 </ul>
 
-                <div>
-                    <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4' >Contact <Image src={assets.arrow_icon} alt="Arrow to contacts" className='w-3' /></a>
+                <div className='flex items-center gap-4'>
+
+                    <button>
+                        <Image src={assets.moon_icon} alt="toggle for theme mode" className='w-6' />
+                    </button>
+
+                    <a href="#contact" className={`hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 ${ovoClass}`} >Contact <Image src={assets.arrow_icon} alt="Arrow to contacts" className='w-3' /></a>
+
+                    <button>
+                        <Image src={assets.menu_black} alt="Hamburger icon for menu toggle" className='w-6' />
+                    </button>
                 </div>
             </nav>
         </>
